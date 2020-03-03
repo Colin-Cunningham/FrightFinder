8e0f01657e7061d7ded4fdd4f212a3819d8ffc19
Colin Cunningham <colin.199643@gmail.com>
2020-03-02 20:43:21 -0500

    added table schema

---
+++ schema.sql
@@ -0,0 +1,23 @@
+-- Creating the Database
+DROP DATABASE IF EXISTS spooky_spots;
+CREATE DATABASE spooky_spots;
+USE DATABASE spooky_spots;
+
+-- Creating Spooks Table
+
+CREATE TABLE spooky_spaces
+(
+	id int NOT NULL AUTO_INCREMENT,
+	city varchar(50) NOT NULL,
+    country varchar(50) NOT NULL,
+	description varchar(10000),
+    location varchar(50) NOT NULL,
+    state varchar(25) NOT NULL,
+    state_ab varchar(2) NOT NULL,
+    lat DECIMAL(14, 14) NOT NULL,
+    long DECIMAL(14, 14) NOT NULL,
+    city_lat DECIMAL(14, 14) NOT NULL,
+    city_long DECIMAL(14, 14) NOT NULL,
+	PRIMARY KEY (id)
+);
+
