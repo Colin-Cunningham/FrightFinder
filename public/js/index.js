c3d75e7a1c6db0f7f3654aa263956b3bf4322124
Jarrod Finn <jarrod.p.finn@gmail.com>
2020-03-09 18:22:20 -0400

    updated formatting error

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $(document).on("click", "#choose", chooseLoc);
   $(document).on("click", "#current", currenLoc);
   $(document).on("click", "#random", randomLoc);
@@ -7,52 +7,50 @@ $(document).ready(function() {
 
 function myFunction(x) {
   x.classList.toggle("fa-thumbs-down");
-};
+}
 
-  function getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-      console.log("Geolocation is not supported by this browser.");
-    }
-  };
-  function showPosition(position) {
-    var lat= position.coords.latitude;
-    var long= position.coords.longitude;
-    return lat, long
-  };
-  function chooseLoc() {
-    var city = $("#input")
-      .val()
-      .trim();
-    $.ajax("/api/choose/" + city, {
-      type: "GET"
-    }).then(function(response) {
-      var dataContainer = $("#col2");
-      dataContainer.empty();
-      for (var i = 0; i < response.length; i++) {
-        dataContainer.append(`<p id="name" style="color: white">${response[i].location}</p> \n 
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    console.log("Geolocation is not supported by this browser.");
+  }
+}
+function showPosition(position) {
+  var lat = position.coords.latitude;
+  var long = position.coords.longitude;
+  return lat, long;
+}
+function chooseLoc() {
+  var city = $("#input")
+    .val()
+    .trim();
+  $.ajax("/api/choose/" + city, {
+    type: "GET"
+  }).then(function (response) {
+    var dataContainer = $("#col2");
+    dataContainer.empty();
+    for (var i = 0; i < response.length; i++) {
+      dataContainer.append(`<p id="name" style="color: white">${response[i].location}</p> \n 
         <p id="desc" style="color:white">${response[i].description} </p> `);
-      }
-    });
-  }
-  function currenLoc() {
-    getLocation()
+    }
+  });
+}
+function currenLoc() {
+  getLocation();
 
-    $.get("/api/current/" + lat + "/" + long, {
-        type: "GET"
-    }).then(function(response){
-        var dataContainer = $("#col2");
-        dataContainer.empty();
-        for (var i = 0; i < response.length; i++) {
-          dataContainer.append(`<p id="name" style="color: white">${response[i].location}</p> \n 
+  $.get("/api/current/" + lat + "/" + long, {
+    type: "GET"
+  }).then(function (response) {
+    var dataContainer = $("#col2");
+    dataContainer.empty();
+    for (var i = 0; i < response.length; i++) {
+      dataContainer.append(`<p id="name" style="color: white">${response[i].location}</p> \n 
           <p id="desc" style="color:white">${response[i].description} </p> `);
-        }
-    })
-  }
-  function fixClass() {
-    $("#desc").toggleClass("hide");
-    console.log("coolio");
-  }
- 
-;
+    }
+  });
+}
+function fixClass() {
+  $("#desc").toggleClass("hide");
+  console.log("coolio");
+};
